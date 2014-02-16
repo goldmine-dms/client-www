2ae86d9375b298a87781c252a6cc9d6717a3b67f
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2014-02-16 15:51:01 +0100

    Minor bug in admin.js

--- admin.js
+++ admin.js
@@ -44,7 +44,7 @@ admin.createuser = function(){
     {
       var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
       var pass = "";
-      for(var x=0;x<10;x++){
+      for(var x=0;x<length;x++){
         var i = Math.floor(Math.random() * chars.length);
         pass += chars.charAt(i);
       }
@@ -77,7 +77,7 @@ admin.createuser = function(){
                 fieldLabel:'Initial Password', 
                 name:'password', 
                 inputType:'text',
-                value: randomPassword() 
+                value: randomPassword(10)
             }
         ],
 
